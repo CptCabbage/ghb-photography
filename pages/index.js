38d57eee9d9b1136b7ff95654c75dcc29729267d
ClaaziX <georgecwnn@hotmail.com>
2022-02-02 17:09:48 +0000

    reset

--- pages/index.js
+++ pages/index.js
@@ -6,31 +6,10 @@ import Link from 'next/link'
 import Date from '../components/date'
 import React from 'react';
 import ReactDOM from 'react-dom';
-var listOfImages =[];
-import background from "./public/images/Lightning.jpg";
 
 export default function Home({ allPostsData }) {
   return (
-    importAll(r) {
-        return r.keys().map(r);
-    }
-        <div style={{ backgroundImage: `url(${background})` }}>
 
-    
-    componentWillMount() {
-        listOfImages = this.importAll(require.context('./images/', false, /\.(png|jpe?g|svg)$/));
-    }
-    render(){
-        return(
-          <div>
-              {
-                    listOfImages.map(
-                      (image, index) =>    <img key={index} src={image} alt="info"></img>
-                    )
-              }
-          </div>
-        )
-    }
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
